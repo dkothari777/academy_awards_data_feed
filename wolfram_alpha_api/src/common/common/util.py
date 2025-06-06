cf1fce74549b7b5ba3019304758af5a848fb2529
Darshan Kothari <dkothari777@gmail.com>
2025-06-05 19:53:07 -0700

    Add multi threading to get all the years for Academy Award Winners

--- wolfram_alpha_api/src/common/common/util.py
+++ wolfram_alpha_api/src/common/common/util.py
@@ -3,7 +3,12 @@ from pathlib import Path
 
 def write_out(module:str, filename:str, data):
     cwd = Path.cwd()
-    target_path = cwd.joinpath(f"data/{module}").mkdir(parents=True, exist_ok=True)
+    cwd.joinpath(f"data/{module}").mkdir(parents=True, exist_ok=True)
     filename = f"data/{module}/{filename}"
     with open(filename, "w") as fs:
         json.dump(data, fs)
+
+def list_files(module: str) -> list[Path]:
+    cwd = Path.cwd()
+    cwd.joinpath().mkdir(parents=True, exist_ok=True)
+    return list(cwd.joinpath(f"data/{module}").glob("*.json"))
