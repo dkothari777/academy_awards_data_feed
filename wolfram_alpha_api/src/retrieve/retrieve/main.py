1035db53ba2738010bf3720f52f9a803d1b12f15
Darshan Kothari <dkothari777@gmail.com>
2025-06-05 19:53:07 -0700

    Add multi threading to get all the years for Academy Award Winners

--- wolfram_alpha_api/src/retrieve/retrieve/main.py
+++ wolfram_alpha_api/src/retrieve/retrieve/main.py
@@ -1,26 +1,32 @@
 import os
+import aiohttp.client_reqrep
 import requests
-from common.util import write_out
+from datetime import datetime
+import asyncio
+import aiohttp
+from common.util import write_out, list_files
+import time
+from pathlib import Path
 
 WOLFRAM_APP_ID=os.environ["WOLFRAM_APP_ID"]
 WOLFRAM_API_URL="https://api.wolframalpha.com/v2/query"
+ACADEMY_AWARDS_START_YEAR=1929 #1929 year that awards started
+QUEUE: list[int] = []
 
-
-def main():
-    year=1935
+async def get_academy_awards(year:int):
+    print(f"Capturing Academy Awards Year: {year}")
     input=f"Academy Awards Nominees {year}"
-    output="json"
-    params = {"input": input, "output": "json", "appid": WOLFRAM_APP_ID, "podstate": "Result__More"}
-    count = 1
+    count = 2
+    params = {"input": input, "output": "json", "appid": WOLFRAM_APP_ID}
     has_more = True
+    json_response = None
     while(has_more):
         try:
-            if count == 1:
-                response = requests.get(WOLFRAM_API_URL, params=params)
-            else:
-                params["podstate"] = f"{count+1}@Result__More"
-                response = requests.get(WOLFRAM_API_URL, params=params)
-            result = response.json()["queryresult"]["pods"][1]
+            params["podstate"] = f"{count}@Result__More"
+            async with aiohttp.ClientSession() as session:
+                async with session.get(WOLFRAM_API_URL, params=params) as response:
+                    json_response = await response.json()
+            result = json_response["queryresult"]["pods"][1]
             has_more = False
             states = result["states"]
             for state in states:
@@ -29,9 +35,36 @@ def main():
                     count +=1
                     has_more = True
         except Exception as e:
+            # print(json_response)
             print(e)
-            break
-    write_out("retrieve", f"AcademyAwards{year}.json", response.json())
+            return False
+    write_out("retrieve", f"AcademyAwards{year}.json", json_response)
+    print(f"Finish collecting Academy awards for {year}.")
+    return True
+
+async def semwrapper(task: int, sem: asyncio.Semaphore) -> bool:
+    async with sem:
+        result = await get_academy_awards(task)
+        return result
+
+def generate_queue(start_year:int, end_year:int) -> list[int]:
+    queue: list[int] = []
+    files: list[Path] = list_files("retrieve")
+    years_persisted: list[int] = []
+    for file in files:
+        year = file.name.removeprefix("AcademyAwards").removesuffix(".json")
+        years_persisted.append(int(year))
+    for year in range(start_year, end_year):
+        if year not in years_persisted:
+            queue.append(year)
+    return queue
+
+async def main():
+    current_year = datetime.now().year + 1
+    sem = asyncio.Semaphore(10)
+    queue: list[int] = generate_queue(ACADEMY_AWARDS_START_YEAR, current_year)
+    async_tasks = [semwrapper(year, sem) for year in queue]
+    await asyncio.gather(*async_tasks)
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
